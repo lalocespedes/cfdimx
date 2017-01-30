16cc62e9d5adc346b949a8bc6294c14b33737212
Eduardo Cespedes <lalocespedes@gmail.com>
2017-01-30 17:51:49 -0600

    cambios

--- src/Sello.php
+++ src/Sello.php
@@ -31,7 +31,19 @@ class Sello
         $namespaces = $sxe->getNamespaces(true);
 
         $XSL = new DOMDocument;
-        $XSL->load( __DIR__ . '/utils/xsltretenciones/retenciones.xslt');
+
+        if(array_keys($namespaces)[0] === "cfdi") {
+
+            $XSL->load( __DIR__ . '/utils/xslt32/cadenaoriginal_3_2.xslt');
+            $c = $xml->getElementsByTagNameNS(array_values($namespaces)[0], 'Comprobante')->item(0);
+        }
+
+
+        if(array_keys($namespaces)[0] === "retenciones") {
+
+            $XSL->load( __DIR__ . '/utils/xsltretenciones/retenciones.xslt');
+            $c = $xml->getElementsByTagNameNS(array_values($namespaces)[0], ucfirst(array_keys($namespaces)[0]))->item(0);
+        }
 
         $proc = new XSLTProcessor;
         $proc->importStyleSheet($XSL);
@@ -41,8 +53,6 @@ class Sello
         openssl_sign($cadena_original, $sig, $private);
 
         $sello = base64_encode($sig);
-
-        $c = $xml->getElementsByTagNameNS(array_values($namespaces)[0], ucfirst(array_keys($namespaces)[0]))->item(0);
 
         if($c->prefix == "retenciones") {
 
