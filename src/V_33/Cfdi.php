bb34ba7c0740bdcb32dd6beae2ba3338ab3ce59d
Eduardo Cespedes <lalocespedes@gmail.com>
2018-01-17 09:50:45 -0600

    cambios

--- src/V_33/Cfdi.php
+++ src/V_33/Cfdi.php
@@ -395,8 +395,9 @@ class Cfdi
             $val = preg_replace('/\s\s+/', ' ', $val);   // Regla 5a y 5c
             $val = trim($val); // Regla 5b
             if (strlen($val)>0) { // Regla 6
-                // $val = str_replace(array('"','>','<'),"'",$val);  // &...;
-                $val = utf8_encode(str_replace("|","/",$val)); // Regla 1
+                $val = str_replace(array('"','>','<'),"'",$val);  // &...;
+                // $val = utf8_encode(str_replace("|","/",$val)); // Regla 1
+                $val = str_replace("|","/",$val); // Regla 1
                 $this->{$node}->setAttribute($key,$val);
             }
         }
